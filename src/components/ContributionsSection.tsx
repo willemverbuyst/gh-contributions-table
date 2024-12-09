fb669c7bd944a516f1fb5263e1a42fda7578f728
Willem Verbuyst <willemverbuyst@gmail.com>
2024-12-09 05:09:01 +0100

    chore: use constants for table setup

--- src/components/ContributionsSection.tsx
+++ src/components/ContributionsSection.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { DAYS_WITH_DAY_NUMBER, MONTHS_WITH_COLSPAN } from "../constants";
 import { getDateMapsForYears } from "../utils/dates";
 import { DataCells } from "./DataCells";
 import { DayCell } from "./DayCell";
@@ -18,64 +19,23 @@ export function ContributionsSection({
           <thead>
             <tr>
               <td></td>
-              <MonthCell month="Jan" colSpan={4} />
-              <MonthCell month="Feb" colSpan={4} />
-              <MonthCell month="Mar" colSpan={5} />
-              <MonthCell month="Apr" colSpan={4} />
-              <MonthCell month="May" colSpan={4} />
-              <MonthCell month="Jun" colSpan={5} />
-              <MonthCell month="Jul" colSpan={4} />
-              <MonthCell month="Aug" colSpan={5} />
-              <MonthCell month="Sep" colSpan={4} />
-              <MonthCell month="Oct" colSpan={4} />
-              <MonthCell month="Nov" colSpan={4} />
-              <MonthCell month="Dec" colSpan={5} />
+              {MONTHS_WITH_COLSPAN.map(({ month, colSpan }) => (
+                <MonthCell key={month} month={month} colSpan={colSpan} />
+              ))}
             </tr>
           </thead>
 
           <tbody>
-            <tr className="h-3 leading-none">
-              <DayCell day="Sun" hidden />
-              <DataCells
-                contributions={contributions.get(selectedYear)?.get(0) ?? []}
-              />
-            </tr>
-            <tr className="h-3 leading-none">
-              <DayCell day="Mon" />
-              <DataCells
-                contributions={contributions.get(selectedYear)?.get(1) ?? []}
-              />
-            </tr>
-            <tr className="h-3 leading-none">
-              <DayCell day="Tue" hidden />
-              <DataCells
-                contributions={contributions.get(selectedYear)?.get(2) ?? []}
-              />
-            </tr>
-            <tr className="h-3 leading-none">
-              <DayCell day="Wed" />
-              <DataCells
-                contributions={contributions.get(selectedYear)?.get(3) ?? []}
-              />
-            </tr>
-            <tr className="h-3 leading-none">
-              <DayCell day="Thu" hidden />
-              <DataCells
-                contributions={contributions.get(selectedYear)?.get(4) ?? []}
-              />
-            </tr>
-            <tr className="h-3 leading-none">
-              <DayCell day="Fri" />
-              <DataCells
-                contributions={contributions.get(selectedYear)?.get(5) ?? []}
-              />
-            </tr>
-            <tr className="h-3 leading-none">
-              <DayCell day="Sat" hidden />
-              <DataCells
-                contributions={contributions.get(selectedYear)?.get(6) ?? []}
-              />
-            </tr>
+            {DAYS_WITH_DAY_NUMBER.map(({ day, dayNumber, display }) => (
+              <tr className="h-3 leading-none" key={day}>
+                <DayCell day={day} hidden={!display} />
+                <DataCells
+                  contributions={
+                    contributions.get(selectedYear)?.get(dayNumber) ?? []
+                  }
+                />
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
--- src/constants.ts
+++ src/constants.ts
@@ -7,3 +7,26 @@ export const BACKGROUND_COLOR = {
   3: "bg-green-500",
   4: "bg-green-600",
 };
+export const MONTHS_WITH_COLSPAN = [
+  { month: "Jan", colSpan: 4 },
+  { month: "Feb", colSpan: 4 },
+  { month: "Mar", colSpan: 5 },
+  { month: "Apr", colSpan: 4 },
+  { month: "May", colSpan: 4 },
+  { month: "Jun", colSpan: 5 },
+  { month: "Jul", colSpan: 4 },
+  { month: "Aug", colSpan: 5 },
+  { month: "Sep", colSpan: 4 },
+  { month: "Oct", colSpan: 4 },
+  { month: "Nov", colSpan: 4 },
+  { month: "Dec", colSpan: 5 },
+];
+export const DAYS_WITH_DAY_NUMBER = [
+  { day: "Sun", dayNumber: 0, display: false },
+  { day: "Mon", dayNumber: 1, display: true },
+  { day: "Tue", dayNumber: 2, display: false },
+  { day: "Wed", dayNumber: 3, display: true },
+  { day: "Thu", dayNumber: 4, display: false },
+  { day: "Fri", dayNumber: 5, display: true },
+  { day: "Sat", dayNumber: 6, display: false },
+];
